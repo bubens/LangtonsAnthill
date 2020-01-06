15dc6f3e6a4edd20487df7dcdd8eabe369245a7f
Henning Beyer <bubens@unpunk.de>
2020-01-06 21:58:11 +0100

    Using my 'standard' make.sh

--- make.sh
+++ make.sh
@@ -1,22 +1,41 @@
 #!/bin/bash
 
-CORE_FILES="./src/langton.ts"
-GUI_FILES="./src/adjust.ts"
+SOURCE_TS="./src/"
+SOURCE_HTML="./html/index.html";
+SOURCE_CSS="./style/style.css";
 
-DEST="./rel"
-CORE="langton.js"
-GUI="adjust.js"
+TMP_FOLDER="./tmp_$(date +%s)";
 
-if [ ! -d "$DEST" ];
-  then
-    mkdir $DEST;
-  else
-    rm $DEST/*;
+RELEASE_FOLDER="./rel";
+
+echo "Step 0: Prepare build process..."
+# Create tmp-directory
+if [ ! -d $TMP_FOLDER ]; then
+    echo "making TMP-Folder..."
+    mkdir $TMP_FOLDER
 fi
 
-tsc --module amd --target "ES2016" --outFile "$DEST/$CORE" --strict --sourceMap $CORE_FILES &&
-echo "core compiled" &&
-tsc --module amd --target "ES2016" --outFile "$DEST/$GUI" --strict --sourceMap  $GUI_FILES &&
-echo "gui compiled" &&
-echo "SUCCESS!"
+if [ ! -d $RELEASE_FOLDER ]; then
+    echo "making release-folder (rel/)..."
+    mkdir $RELEASE_FOLDER
+else
+    echo "cleaning up release-folder (rel/*)..."
+    rm $RELEASE_FOLDER/*
+fi
+
+# compile
+echo "Step 1: Copying html & css..." &&
+cp -v $SOURCE_HTML $RELEASE_FOLDER &&
+cp -v $SOURCE_CSS $RELEASE_FOLDER &&
+echo "Step 2: Compiling TS..." &&
+tsc --module commonjs --target "ES2016" --outDir $TMP_FOLDER $SOURCE_TS/* &&
+echo "Step 3: Bundling up with native parts..." &&
+browserify -o $TMP_FOLDER/app.js $TMP_FOLDER/main.js &&
+cp $TMP_FOLDER/app.js $RELEASE_FOLDER &&
+echo "Done building app"
+
+echo "Cleaning up..." &&
+rm -r $TMP_FOLDER &&
+echo "Success"
+
 
